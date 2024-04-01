4aae565648e0131f8ac88f78a647946a5a46d866
dallyy <114714553+dallyy@users.noreply.github.com>
2024-04-01 18:18:23 +0800

    Create 开始模板.cpp

---
+++ 开始模板.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <stdlib.h>
+
+#define fi first
+#define se second
+//#define IOS ios::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define LL long long
+
+using namespace std;
+
+typedef pair<int, int> PII;
+
+const int N = 1e5;
+
+void solve() {
+	
+}
+
+int main(){
+	//IOS ;
+
+	// int T;
+	// cin >> T;
+	// while (T--) {
+	// 	solve();
+	// }
+	solve();
+
+	return 0;
+}
